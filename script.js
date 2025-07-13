5f83901ad7f6212b6fdd85854c6e09bf26eec78c
Isabella Harrington <isabellaharrington643@gmail.com>
2025-07-13 16:59:22 -0400

    Added operate function that takes parameters for number1, number2, and operator
    The function then uses these parameters to determine
    which mathematical function will be run (and with
    which numbers)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operand1 = 0;
+let operand2 = 0;
+let operator = "";
+
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -14,3 +18,14 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
+function operate(num1, num2, sign) {
+    if (sign == "+") {
+        return add(num1, num2);
+    } else if (sign == "-") {
+        return subtract(num1, num2);
+    } else if (sign == "*") {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2);
+    };
+};
