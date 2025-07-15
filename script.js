b3c833496e174538b57343ffb0619deb7ce9e39f
Isabella Harrington <isabellaharrington643@gmail.com>
2025-07-14 22:06:53 -0400

    Added event listeners for number button clicks to append numbers to the input text box
    Created a numCount to avoid numbers overflowing the
    calculator
    
    Created an operatorCount to ensure that only two
    numbers could be added/subtracted/multiplied/
    divided at once

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ const subtractButton = document.getElementById('subtract-button');
 const multiplyButton = document.getElementById('multiply-button');
 const divideButton = document.getElementById('divide-button');
 const equalsButton = document.getElementById('equals-button');
-const clearButton = document.getElemnentById('clear-button');
+const clearButton = document.getElementById('clear-button');
 
+let numCount = 0;
 let operand1 = 0;
 let operand2 = 0;
 let operator = "";
+let operatorCount = 0;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -47,3 +49,156 @@ function operate(num1, num2, sign) {
         return divide(num1, num2);
     };
 };
+
+function calculate() {
+    numCount = 0;
+
+    if (inputTextBox.textContent != "") {
+        operand2 = parseInt(inputTextBox.textContent);
+    } else {
+        operand2 = 0;
+    };
+
+    if (Number.isInteger(operate(operand1, operand2, operator))) {
+        inputTextBox.textContent = String(operate(operand1, operand2, operator));
+    } else {
+        inputTextBox.textContent = String((operate(operand1, operand2, operator).toFixed(3)));
+    }
+    
+    operatorCount = 0;
+};
+
+numberZeroButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('0');
+        numCount += 1;
+    };
+});
+
+numberOneButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('1');
+        numCount += 1;
+    };
+});
+
+numberTwoButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('2');
+        numCount += 1;
+    };
+});
+
+numberThreeButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('3');
+        numCount += 1;
+    };
+});
+
+numberFourButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('4');
+        numCount += 1;
+    };
+});
+
+numberFiveButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('5');
+        numCount += 1;
+    };
+});
+
+numberSixButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('6');
+        numCount += 1;
+    };
+});
+
+numberSevenButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('7');
+        numCount += 1;
+    };
+});
+
+numberEightButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('8');
+        numCount += 1;
+    };
+});
+
+numberNineButton.addEventListener('click', () => {
+    if (numCount < 10) {
+        inputTextBox.append('9');
+        numCount += 1;
+    };
+});
+
+addButton.addEventListener('click', () => {
+    operator = "+";
+
+    if (operatorCount < 1) {
+        numCount = 0;
+
+        operand1 = parseInt(inputTextBox.textContent);
+        inputTextBox.textContent = '';
+        operatorCount += 1;
+    } else {
+        calculate();
+    }
+});
+
+subtractButton.addEventListener('click', () => {
+    operator = "-";
+    numCount = 0;
+
+    if (operatorCount < 1) {
+        numCount = 0;
+
+        operand1 = parseInt(inputTextBox.textContent);
+        inputTextBox.textContent = '';
+        operatorCount += 1;
+    } else {
+        calculate();
+    }
+});
+
+multiplyButton.addEventListener('click', () => {
+    operator = "*";
+    numCount = 0;
+
+    if (operatorCount < 1) {
+        numCount = 0;
+
+        operand1 = parseInt(inputTextBox.textContent);
+        inputTextBox.textContent = '';
+        operatorCount += 1;
+    } else {
+        calculate();
+    };
+});
+
+divideButton.addEventListener('click', () => {
+    operator = "/";
+    numCount = 0;
+
+    if (operatorCount < 1) {
+        numCount = 0;
+        
+        operand1 = parseInt(inputTextBox.textContent);
+        inputTextBox.textContent = '';
+        operatorCount += 1;
+    } else {
+        calculate();
+    };
+});
+
+equalsButton.addEventListener('click', calculate);
+
+clearButton.addEventListener('click', () => {
+    inputTextBox.textContent = '';
+});
